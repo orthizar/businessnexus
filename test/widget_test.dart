5b5428a95fce042458aaf5d5f78dcb205a730915
Silvan Kohler <silvan@famkohler.com>
2023-06-10 18:40:11 +0200

    edited some widget tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,34 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:businessnexus/screens/overview_screen.dart';
- void main() {
-  group('OverviewScreenState', () {
-    testWidgets('should display a list of companies', (WidgetTester tester) async {
-      // Arrange
-      await tester.pumpWidget(const MaterialApp(home: OverviewScreen()));
-       // Assert
-      expect(find.byType(ListTile), findsNWidgets(3));
+import 'package:get/get.dart';
+
+void main() {
+  group('OverviewScreen', () {
+    testWidgets('should display an empty list of companies',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(const GetMaterialApp(home: OverviewScreen()));
+
+      expect(find.byType(ListTile), findsNWidgets(0));
     });
-    //  testWidgets('should navigate to business details screen when a company is tapped', (WidgetTester tester) async {
-    //   // Arrange
-    //   await tester.pumpWidget(const MaterialApp(home: OverviewScreen()));
+    // testWidgets('should display a list of companies',
+    //     (WidgetTester tester) async {
+    //   await tester.pumpWidget(const GetMaterialApp(home: OverviewScreen()));
+    //   await tester.tap(find.widgetWithIcon(FloatingActionButton, Icons.add));
+    //   await tester.pumpAndSettle();
+    //   await tester.enterText(find.byElementType(TextField), "Company A");
+    //   await tester.tap(find.widgetWithText(ElevatedButton, "Create Company"));
+    //   await tester.pumpAndSettle();
+    //   expect(find.byType(ListTile), findsNWidgets(1));
+    // });
+    // testWidgets(
+    //     'should navigate to business details screen when a company is tapped',
+    //     (WidgetTester tester) async {
+    //   await tester.pumpWidget(const GetMaterialApp(home: OverviewScreen()));
+    //   var fab = find.widgetWithIcon(FloatingActionButton, Icons.add);
+    //   print(fab);
+    //   await tester.tap(fab);
+    //   await tester.pumpAndSettle();
+    //   print(find.byElementType(TextField));
+    //   await tester.
+    //   await tester.enterText(find.byElementType(TextField), "Company A");
+    //   await tester.tap(find.widgetWithText(ElevatedButton, "Create Company"));
+    //   await tester.pumpAndSettle();
     //   final companyTileFinder = find.widgetWithText(ListTile, 'Company A');
-    //    // Act
     //   await tester.tap(companyTileFinder);
     //   await tester.pumpAndSettle();
-    //    // Assert
-    //   expect(find.text('Business Details'), findsOneWidget);
+    //   expect(find.text('Level: 1'), findsOneWidget);
+    //   expect(find.text('Revenue: 0'), findsOneWidget);
     // });
-     testWidgets('should display a new company when _createNewCompany is called', (WidgetTester tester) async {
-      // Arrange
-      await tester.pumpWidget(const MaterialApp(home: OverviewScreen()));
-      final initialLength = find.byType(ListTile).evaluate().length;
-       // Act
-      await tester.tap(find.byType(FloatingActionButton));
-      await tester.pumpAndSettle();
-       // Assert
-      expect(find.byType(ListTile), findsNWidgets(initialLength + 1));
-      expect(find.widgetWithText(ListTile, 'New Company'), findsOneWidget);
-    });
   });
 }
