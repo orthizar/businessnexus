9c0e4213ce75fe0ed1193489cb7ec7f8ee449e79
Silvan Kohler <silvan@famkohler.com>
2023-06-11 14:58:30 +0200

    test: adjusted tests to the new design

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,15 +1,15 @@
+import 'package:businessnexus/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:businessnexus/screens/overview_screen.dart';
-import 'package:get/get.dart';
 
 void main() {
   group('OverviewScreen', () {
     testWidgets('should display an empty list of businesses',
         (WidgetTester tester) async {
-      await tester.pumpWidget(GetMaterialApp(home: OverviewScreen()));
+      await tester.pumpWidget(BusinessNexusApp());
 
-      expect(find.byType(ListTile), findsNWidgets(0));
+      expect(find.byType(ListTile), findsOneWidget);
+      expect(find.text("No businesses yet."), findsOneWidget);
     });
   });
 }
